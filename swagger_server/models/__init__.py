86eaff9c9cb9ce0d2e8ae08d7073450a49e3cd9f
stopcontact <sander.vrijenhoek@student.uva.nl>
2023-02-16 14:52:20 +0100

    api with logic

---
+++ swagger_server/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.grade_records import GradeRecords
+from swagger_server.models.student import Student
